70703268b8044d207bce85b555c9bd42bf854699
Tim Detwiler <timdetwiler@gmail.com>
2012-05-30 11:56:28 -0400

    Add RoR Project Files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HcfIo::Application.config.secret_token = '34de57840b6f423ea01352807ac6c142c13a5d5704e7337f1a099b19da8be77d1a90ec101b74398b0733d5b0f8ac4de3abbf312ffd1c631579ef588f1da2ac17'
